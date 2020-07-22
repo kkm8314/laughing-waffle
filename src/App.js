0e7e8fdac6e26025de888c74fa4c87f976321960
Aaron Kim <kkm8314@naver.com>
2020-07-22 20:21:49 +0900

    [fix] 영화 웹사이트 만들어보기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food(props) {
+  return <h1> i like {props.fav} </h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      하이
+      <Food fav="kimchi" />
+      <Food fav="kimbab" />
+      <Food fav="namul" />
+      <Food fav="bab" />
     </div>
   );
 }
